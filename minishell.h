cb45e988ef69cf42cbd308539a503663e6972aea
cocozhuk <zhuke1127@gmail.com>
2026-04-15 14:21:21 +0200

    pp

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: cocozhu <cocozhu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/30 17:07:38 by zkarman           #+#    #+#             */
-/*   Updated: 2026/04/15 14:01:31 by cocozhu          ###   ########.fr       */
+/*   Updated: 2026/04/15 14:20:23 by cocozhu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,25 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
-//pour parsing
-typedef struct s_token
+typedef struct  s_shell
+{
+    char    **envp_copy;
+    t_cmd   *command_list;
+    int     last_exit_val;
+}   t_shell;
+typedef struct s_cmd
 {
     char    **commands;
     t_cmd   *next;
-    t_shell     *shell;
+    t_shell *shell;
 }   t_cmd;
+
+typedef struct s_token
+{
+    char    *value;
+    int     type;
+    struct s_token  *next;
+}  t_token;
 
 int 	build_token(t_token **input_list, char *input);
 int	    append_node(t_token **input_list, char *token);
@@ -36,10 +48,4 @@ void    reading_commands(t_cmd *command_list);
 int 	is_space(char c);
 void    free_tokens(t_token **tokens);
 
-/*typedef struct s_cmd
-{
-    char    **commands; //Just split each command into a **arr instead of separate components. I need to pass a ** to execve to execute the function
-    t_cmd   *next;
-}   t_cmd ;
-*/
 #endif
