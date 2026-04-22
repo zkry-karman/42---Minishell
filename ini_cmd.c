b0b879af55685515233b1f61b5ee555822e797d4
cocozhuk <zhuke1127@gmail.com>
2026-04-23 00:21:54 +0200

    updated the cmd struct

--- ini_cmd.c
+++ ini_cmd.c
@@ -6,9 +6,28 @@
 /*   By: cocozhu <cocozhu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/16 11:55:10 by cocozhu           #+#    #+#             */
-/*   Updated: 2026/04/16 13:32:37 by cocozhu          ###   ########.fr       */
+/*   Updated: 2026/04/22 23:17:51 by cocozhu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int	build_cmd(t_shell *shell)
+{
+	t_token *cur;
+
+	cur = shell->input_list;
+	while (cur != NULL)
+	{
+		
+		while (is_space(input[i]) == 1)
+			i++;
+		if (input[i] == '\0')
+			break ; 
+		cur_token = extract_token(shell, input, &i);
+		if (cur_token == NULL)
+			return (1);
+		append_node(&(shell->cmds), cur);
+	}
+	return (0);
+}
