437c40c36d537797da9eebce2ce236f2ccf22c99
zkarman <zkarman@f1r5s7.paris.42.school>
2026-04-28 16:03:51 +0200

    SHES WORKING

--- tool_box_execution.c
+++ tool_box_execution.c
@@ -3,14 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   tool_box_execution.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kzhu@student.42.fr <kzhu>                  +#+  +:+       +#+        */
+/*   By: zkarman <zkarman@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/16 12:27:28 by cocozhu           #+#    #+#             */
-/*   Updated: 2026/04/28 14:20:49 by kzhu@student.42.f###   ########.fr       */
+/*   Updated: 2026/04/28 16:01:49 by zkarman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int     envp_size(t_env *envp)
+{
+    int     i;
+    
+    i = 0;
+    while (envp)
+    {
+        i++;
+        envp = envp->next;
+    }
+    return (i);
+}
 
 char    **envp_list_to_arr(t_shell *shell)
 {
@@ -20,7 +33,7 @@ char    **envp_list_to_arr(t_shell *shell)
     char    *temp;
 
     curr = shell->env_list;
-    arr = malloc(sizeof(char *) * (ft_lstsize((t_list *)curr) + 1));
+    arr = malloc(sizeof(char *) * envp_size(curr) + 1);
     if (!arr)
         return (NULL);
     i = 0;
@@ -34,4 +47,19 @@ char    **envp_list_to_arr(t_shell *shell)
     }
     arr[i] = NULL;
     return (arr);
+}
+
+int     command_count(t_cmd *cmds)
+{
+    int     i;
+    t_cmd   *tmp;
+    
+    i = 0;
+    tmp = cmds;
+    while (tmp)
+    {
+        i++;
+        tmp = tmp->next;
+    }
+    return (i);
 }
