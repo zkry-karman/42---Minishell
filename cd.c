b8c0b9fd3dbfef8a2169eaa263eb1b2656c22785
Zachary Karman <zkarman@f6r2s15.clusters.42paris.fr>
2026-04-29 11:16:15 +0200

    finished cd builtin

--- cd.c
+++ cd.c
@@ -3,58 +3,57 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: karmanz <karmanz@student.42.fr>            +#+  +:+       +#+        */
+/*   By: zkarman <zkarman@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/28 20:51:19 by karmanz           #+#    #+#             */
-/*   Updated: 2026/04/28 23:11:08 by karmanz          ###   ########.fr       */
+/*   Updated: 2026/04/29 11:15:00 by zkarman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void    swap_dir(t_shell *shell, t_env *oldpwd)
-{
-    // dont swap pointers. need to rewrite this function
-    t_env   *curr;
-    char    *tmp;
-
-    curr = get_env_node(shell, "PWD");
-    tmp = curr->value;
-    curr->value = oldpwd->value;
-    oldpwd->value = tmp;
-}
-
 int     change_dir(t_shell *shell, char **args)
 {
     t_env    *target_node;
+    char    *new_path;
+    char    cwd[1024];
     
-    if (args[2])
+    
+    if (args[1] && args[2])
         return (ft_putstr_fd("Too many argumants\n", 2), 1);
     if (args[1] == NULL)
     {
         target_node = get_env_node(shell, "HOME");
-        chdir(target_node->value);
+        if (!target_node)
+            return (ft_putstr_fd("minishell: cd: HOME not set\n", 2), 1);
+        new_path = target_node->value;
+        printf("%s\n", new_path);
     }
     else if (ft_strcmp(args[1], "-") == 0)
     {
         target_node = get_env_node(shell, "OLDPWD");
         if (!target_node)
             return(ft_putstr_fd("minishell: cd: OLDPWD not set", 2), 1);
-        printf("%s\n", target_node->value);
-        swap_dir(shell, target_node);
+        new_path = target_node->value;
+        printf("%s\n", new_path);
     }
     else
+        new_path = args[1];
+    if (!getcwd(cwd, sizeof(cwd)))
+        return (1);
+    if (chdir(new_path) == -1)
     {
-        target_node = get_env_node(shell, "OLDPWD");
-        if (access(args[1], X_OK) == 0)
-            return 1;
-        if (chdir(args[1]) == -1)
-        {
-            ft_putstr_fd("minishell: cd: No such file or directory", 2);
-            shell->exit_status = 1;
-            return (1);
-        }
-        swap_dir(shell, target_node);
+        perror("minishell: cd");
+        return (1);
+    }
+    target_node = get_env_node(shell, "OLDPWD");
+    if (target_node)
+        replace_env_value(target_node, cwd);
+    if (getcwd(cwd, sizeof(cwd)))
+    {
+        target_node = get_env_node(shell, "PWD");
+        if (target_node)
+            replace_env_value(target_node, cwd);
     }
     return (0);
 }
